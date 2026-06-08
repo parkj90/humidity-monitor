7af6aaf304adafcf36ba527962c299ac2062ceab
jpark <JamesYPark90@gmail.com>
2026-06-08 16:34:01 -0700

    add ESP32 config

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "sdkconfig.h"
+
+#define I2C_MASTER_SCL_IO            CONFIG_I2C_MASTER_SCL
+#define I2C_MASTER_SDA_IO            CONFIG_I2C_MASTER_SDA
+#define I2C_MASTER_FREQ_HZ           CONFIG_I2C_MASTER_FREQUENCY
 
 void app_main(void)
 {
